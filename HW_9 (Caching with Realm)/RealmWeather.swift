32a14a86417fb5d813207436a9a68c80b9d786be
Liza-R <83958561+Liza-R@users.noreply.github.com>
2021-08-17 08:36:11 +0300

    Code porting and Storyboard for WeatherVC and creating Realm Class

---
+++ HW_9 (Caching with Realm)/RealmWeather.swift
@@ -0,0 +1,13 @@
+//
+//  RealmWeather.swift
+//  HW_9 (Caching with Realm)
+//
+//  Created by Elizaveta Rogozhina on 13.08.2021.
+//
+
+import Foundation
+import RealmSwift
+
+class RealmWeather{
+    
+}
